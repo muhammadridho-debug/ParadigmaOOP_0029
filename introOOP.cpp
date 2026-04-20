ade4a36e37fa5f189f00498745ff57922b6703d1
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-20 10:43:02 +0700

    membuat isi untuk method printData

--- introOOP.cpp
+++ introOOP.cpp
@@ -9,6 +9,8 @@ public :
     float nilai;
 
     void printData(){
-
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
     }
 };
